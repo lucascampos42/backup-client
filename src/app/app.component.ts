efb208307c7b2423f6514aa5938abcb4e4030fb3
Lucas Campos <bhlucascampos@gmail.com>
2024-12-08 09:55:23 -0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ConfigComponent } from './config/config.component';
 import { MenuComponent } from './menu/menu.component';
