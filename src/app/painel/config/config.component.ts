d2543de2747e812080040fa7ad7f5be85d85ef37
Lucas Campos <bhlucascampos@gmail.com>
2024-12-15 17:28:22 -0300

    novo html de diretorios

--- src/app/painel/config/config.component.ts
+++ src/app/painel/config/config.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
     selector: 'app-config',
     templateUrl: './config.component.html',
     styleUrls: ['./config.component.scss'],
     imports: [
-        FormsModule,
+      FormsModule,
+      MatIcon
     ]
 })
 export class ConfigComponent {}
