5a154cf9b289cf7edb5f3a27ee25a5bea8b0fb6d
Lucas Campos <31932085+lucascampos42@users.noreply.github.com>
2025-06-21 18:47:53 -0300

    reinicio

---
+++ .angular/cache/19.0.4/backup-client/vite/deps_temp_aa9b570c/@tauri-apps_api_tauri.js
@@ -0,0 +1,12 @@
+import {
+  convertFileSrc,
+  invoke,
+  transformCallback
+} from "./chunk-Q4VBDLF2.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  convertFileSrc,
+  invoke,
+  transformCallback
+};
+//# sourceMappingURL=@tauri-apps_api_tauri.js.map
